da8908b0751eca7ba556ee2ab92b5eea71175f46
__Rawan__ <rawanroni2003@gmail.com>
2024-05-03 18:37:40 +0300

    added comments and some modifications in problem1

--- Problem1.pl
+++ Problem1.pl
@@ -1,282 +1,217 @@
-% % [[yellow, yellow, yellow, red], [blue, yellow, blue, yellow], [blue, blue, blue, yellow], [blue, blue, blue, yellow]]
-
-% detects if no cycle
-% if two or more cycles exist print all
-
-% Predicate to set the value of global variable N
+% Predicate to set the value of global variable N.
 set_n(Value) :-
     retractall(n(_)),
     assertz(n(Value)).
 
 
-% Predicate to get the value of global variable N
+% Predicate to get the value of global variable N.
 get_n(Value) :-
     n(Value).
 
 
-% Predicate to set the value of global variable M
+% Predicate to set the value of global variable M.
 set_m(Value) :-
     retractall(m(_)),
     assertz(m(Value)).
 
 
-% Predicate to get the value of global variable M
+% Predicate to get the value of global variable M.
 get_m(Value) :-
     m(Value).
 
 
-% Predicate to set the value of the list of lists
+% Predicate to set the value of the board.
 set_board(Value) :-
     retractall(board(_)),
     assertz(board(Value)).
 
 
-% Predicate to get the value of the list of lists
+% Predicate to get the value of the board.
 get_board(Value) :-
     board(Value).
 
 
+% base case for search -> if current node has no valid children (valid children means that they are adjacents of the current node and has the same color of the current node) -> no cycle.
+search(Open, Closed, _):-
+    getState(Open, Node, _), % get the current node from the open list.
+    getChildren(Node, Children1, Children2), % get the children of the current node -> Children1 and Children2 hence if the node in the cycle it should return at least 2 children.
+    checkNextState(Children1, Closed, ValidChildren1), % check that nodes in Children1 not in closed list and uniques -> return ValidChildren1.
+    checkNextState(Children2, Closed, ValidChildren2), % check that nodes in Children2 not in closed list and uniques -> return ValidChildren2.
+    ValidChildren1 = [], % check if ValidChildren1 is empty.
+    ValidChildren2 = []. % check if ValidChildren2 is empty.
+
+
+% base case for search -> if one of the current node's children is the head of the cycle list -> cycle found.
 search(Open, Closed, CycleList):-
-    getState(Open, Node, TmpOpen), % Step 1
-    TmpOpen = [],
-    % Open = [],
-    % getAllValidChildren(Node, TmpOpen, NewClosed, Children1, Children2), % Step3
-    % Children1 = [],
-    % Children2 = [],
-    % length(CycleList, Length),
-    % Length >= 4,
-    % CurrentState = Goal, !, % Step 2 -> need to handle the check for the goal
-    % write("Search is complete!"), nl,
-    write([Node|CycleList]).
-    % printSolution(Closed). % -> modify the print.
-    % print_cycles(Closed).    
-
-% print_cycles(Closed):-
-%     [[X, Y, Color], [X, Y+1, Color], [X+1, Y+1, Color], [X+1, Y, Color]] = Closed.
+    getState(Open, Node, _), % get the current node from the open list.
+    append(Closed, [Node], _), % append the current node to the closed list.
+    getChildren(Node, Children1, Children2), % get the children of the current node.
+    [H|_] = CycleList, % get the head of the cycle list.
+    (checkCycle(Children1, H); checkCycle(Children2, H)), % check if any of the children is the head of the cycle list.
+    not(member(Node, CycleList)), % check that current node is not in the cycle list.
+    append(CycleList, [Node], NewCycleList), % append the current node to the cycle list.
+    length(NewCycleList, Length), % get the length of the cycle list.
+    Length >= 4, % check that the length is greater than or equal 4. (smallest size of the cycle is 4).
+    write("Cycle found"), nl,
+    write(NewCycleList), nl.
 
 
-
+% recursive search predicate.
 search(Open, Closed, CycleList):-
-    getState(Open, CurrentNode, TmpOpen),
-    append(Closed, [CurrentNode], NewClosed), % Step 5.1
-    getAllValidChildren(CurrentNode, TmpOpen, NewClosed, Children1, Children2), % Step3
+    getState(Open, CurrentNode, TmpOpen), % get the current node from the open list.
+    append(Closed, [CurrentNode], NewClosed), % append the current node to the closed list.
+    getChildren(CurrentNode, Children1, Children2), % get the children of the current node.
     
     % Check if both Children1 and Children2 are not empty
-    (Children1 \= [], Children2 \= [] ->
-        checkNextState(Children1, Open, NewClosed, ValidChildren1),
-        addChildren(ValidChildren1, TmpOpen, NewOpen), % Step 4
-        checkNextState(Children2, NewOpen, NewClosed, ValidChildren2),
-        addChildren(ValidChildren2, NewOpen, NewOpen2), % Step 4
-        append(CycleList, [CurrentNode], NewCycleList), % Add CurrentNode to CycleList
-        
-        write("Both children are not empty"),
-        write("current node" + CurrentNode), nl,
-        write("children" + Children1 + Children2), nl,
-        write("Closed" + NewClosed), nl,
-        write("Open" + NewOpen2), nl,
-        write("Cycle List" + NewCycleList), nl,
-        
-        % Continue search with CycleList and NewClosed
-        search(NewOpen2, NewClosed, NewCycleList);
-    % If at least one of Children1 or Children2 is empty
-    append(Closed, [CurrentNode], UpdatedClosed),
-    addChildren(Children1, TmpOpen, NewOpen), % Step 4
-    addChildren(Children2, NewOpen, NewOpen2), % Step 4
-    
-    write("current node" + CurrentNode), nl,
-    write("children" + Children1 + Children2), nl,
-    write("Closed" + UpdatedClosed), nl,
-    write("Open" + NewOpen2), nl,
-    write("Cycle List" + CycleList), nl,
-    
-    search(NewOpen2, UpdatedClosed, CycleList)).
-
-    % addChildren(Children1, TmpOpen, NewOpen), % Step 4
-    % addChildren(Children2, NewOpen, NewOpen2), % Step 4
-    % search(NewOpen2, NewClosed). % Step 5.2
+    Children1 \= [], Children2 \= [] ->
+        not(member(CurrentNode, CycleList)), % append the current node to the cycle list.
+        append(CycleList, [CurrentNode], NewCycleList), % Add Current Node to cycle List.
+        checkNextState(Children1, NewClosed, ValidChildren1), % check that nodes in Children1 not in closed list and uniques -> return ValidChildren1.
+        addChildren(ValidChildren1, TmpOpen, NewOpen), % add the valid children to the open list.
+        checkNextState(Children2, NewClosed, ValidChildren2), % check that nodes in Children2 not in closed list and uniques -> return ValidChildren2.
+        addChildren(ValidChildren2, NewOpen, NewOpen2), % add the valid children to the open list.
+        search(NewOpen2, NewClosed, NewCycleList). % continue search with the NewCycleList, NewClosed, and NewOpen.
 
 
-% Implementation of step 3 to get the next states
-% getAllValidChildren(Node, Open, Closed, Children):-
-    % findall([Next1, Next2], getNextState(Node, Open, Closed, Next1, Next2), Children).
-    
-% getAllValidChildren(Node, Open, Closed, Next1List, Next2List):-
-%     findall(Next1, getNextState(Node, Open, Closed, Next1, _), Next1List),
-%     findall(Next2, getNextState(Node, Open, Closed, _, Next2), Next2List).
+% base case of checkCycle if we found that there is a node matches tha first node in the cycle list.
+checkCycle([H|_], H).
 
-getAllValidChildren(Node, Open, Closed, Next1List, Next2List):-
-    findall(Next1-Next2, getNextState(Node, Open, Closed, Next1, Next2), NextPairs),
-    pairs_keys_values(NextPairs, Next1List, Next2List).
+% checkCycle predicate -> check if any node in the list is the first node in the cycle list.
+checkCycle([_|T], H2):-
+    checkCycle(T, H2).
 
-% getNextState(Node, Open, Closed, Next1, Next2):-
-%     move(Node, Next1, Next2),
-%     write("Next1" + Next1),
-%     write("Next2" + Next2),
-%     not(member(Next1, Open)),
-%     not(member(Next1, Closed)),
-%     not(member(Next2, Open)),
-%     not(member(Next2, Closed)).
 
-getNextState(Node, Open, Closed, Next1, Next2):-
+% get children of the node (adjacent nodes and with the same color) returns two lists because the valid node should has at least 2 children.
+getChildren(Node, Next1List, Next2List):-
+    findall(Next1, getNextState(Node, Next1, _), Next1List),
+    findall(Next2, getNextState(Node, _, Next2), Next2List).
+
+% get the valid next state to the current state.
+getNextState(Node, Next1, Next2):-
     move(Node, Next1, Next2).
-    % checkNextState(Next1, Open, Closed),
-    % checkNextState(Next2, Open, Closed).
-
-% Predicate to check if a next state is valid
-% checkNextState(Next, Open, Closed):-
-%     not(member(Next, Open)),
-%     not(member(Next, Closed)).
 
 
-% Modified checkNextState to remove duplicates from the children list
-checkNextState(Children, Open, Closed, ValidChildren) :-
-    % Remove duplicates from Children
-    sort(Children, UniqueChildren),
-    % Check each unique child node
-    checkUniqueChildren(UniqueChildren, Open, Closed, [], ValidChildren).
+% checkNextState to remove duplicates from the children list
+checkNextState(Children, Closed, ValidChildren) :-
+    sort(Children, UniqueChildren), % remove duplicates from Children
+    checkUniqueChildren(UniqueChildren, Closed, [], ValidChildren). % Check each unique child node that it is not in closed list.
 
 
 % Predicate to check each unique child node
-checkUniqueChildren([], _, _, Acc, Acc).
-checkUniqueChildren([Child|Rest], Open, Closed, Acc, ValidChildren) :-
-    % Check if the child is not in Open or Closed
-    (not(member(Child, Open)), not(member(Child, Closed))) ->
-        % If not in Open or Closed, add it to the accumulator
-        checkUniqueChildren(Rest, Open, Closed, [Child|Acc], ValidChildren);
-    % If the child is in Open or Closed, skip it
-    checkUniqueChildren(Rest, Open, Closed, Acc, ValidChildren).
+checkUniqueChildren([], _, Acc, Acc).
+
+checkUniqueChildren([Child|Rest], Closed, Acc, ValidChildren) :-
+    (not(member(Child, Closed))) -> % Check if the child is not in Closed.
+        checkUniqueChildren(Rest, Closed, [Child|Acc], ValidChildren); % If not in Closed, add it to the accumulator
+
+    checkUniqueChildren(Rest, Closed, Acc, ValidChildren). % If the child is in Closed, skip it.
 
 
-% Implementation of getState and addChildren determine the search alg.
-% BFS
+% getState predicate returns the first node in the open list (the approach we follow is LIFO (DFS) so we append in the first of the open list and take the last node appended).
 getState([CurrentNode|Rest], CurrentNode, Rest).
 
-
+% addChildren predicate add the Children to the open list.
 addChildren(Children, Open, NewOpen):-
-    append(Open, Children, NewOpen).
+    append(Children,Open, NewOpen).
 
 
-printSolution([]).
-
-
-printSolution([H|T]):-
-  write(H), nl,
-  printSolution(T).
-
-
-% move(Node, Next):-
-    % (up(Node, Next), left(Node, Next));
-    % (up(Node, Next), right(Node, Next));
-    % (down(Node, Next), right(Node, Next));
-    % (down(Node, Next), left(Node, Next));
-
+% move predicate check the up and left of the current node and return Next1 and Next2 if the node has an up and left nodes.
 move(Node, Next1, Next2):-
     (up(Node, Next1), left(Node, Next2)).
 
+% move predicate check the up and right of the current node and return Next1 and Next2 if the node has an up and right nodes.
 move(Node, Next1, Next2):-
     (up(Node, Next1), right(Node, Next2)).
 
+% move predicate check the down and right of the current node and return Next1 and Next2 if the node has an down and right nodes.
 move(Node, Next1, Next2):-
     (down(Node, Next1), right(Node, Next2)).
 
+% move predicate check the down and left of the current node and return Next1 and Next2 if the node has an down and left nodes.
 move(Node, Next1, Next2):-
     (down(Node, Next1), left(Node, Next2)).
 
 
+% left predicate checks the left node af the current node and if it is valid -> return it.
 left(Node, Next):-
-  get_board(Board),
-  get_n(N),
-  nth0(Indx, Board, Node),
-  Node = [X1, Y1, Color1],
-  CheckIndx is Indx mod N,
-  CheckIndx \= 0,
-  NextIndx is Indx - 1,
-  nth0(NextIndx, Board, Next),
-  Next = [X2, Y2, Color2],
-  write("left"), nl,
-  write(Node), write(" "), write(Next), write(" "), nl,
-  Color1 == Color2.
-%   write(Next),nl.
+    get_board(Board), % get the board.
+    get_n(N), % get N (number of raws).
+    nth0(Indx, Board, Node), % get the index of the current node in the board.
+    Node = [_, _, Color1], % get the Color of the current node.
+    CheckIndx is Indx mod N, % calculate Index % N
+    CheckIndx \= 0, % check that the result != 0 (not in the left border).
+    NextIndx is Indx - 1, % calculate indx of the next node which is currentIndx - 1.
+    nth0(NextIndx, Board, Next), % get the index of the next node.
+    Next = [_, _, Color2], % get the Color of the nextNode.
+    Color1 == Color2. % check that both are the same.
 
 
 right(Node, Next):-
-  get_board(Board),
-  get_n(N),
-  nth0(Indx, Board, Node),
-  Node = [X1, Y1, Color1],
-  CheckIndx is (Indx+1) mod N,
-  CheckIndx \= 0,
-  NextIndx is Indx + 1,
-  nth0(NextIndx, Board, Next),
-  Next = [X2, Y2, Color2],
-  write("right"), nl,
-  write(Node), write(" "), write(Next), write(" "), nl,
-  Color1 == Color2.
-%   write(Next),nl.
+    get_board(Board), % get the board.
+    get_n(N), % get N (number of raws).
+    nth0(Indx, Board, Node), % get the index of the current node in the board.
+    Node = [_, _, Color1], % get the Color of the current node.
+    CheckIndx is (Indx+1) mod N, % calculate (Index+1) % N
+    CheckIndx \= 0, % check that the result != 0 (not in the right border).
+    NextIndx is Indx + 1, % calculate indx of the next node which is currentIndx + 1.
+    nth0(NextIndx, Board, Next), % get the index of the next node.
+    Next = [_, _, Color2], % get the Color of the nextNode.
+    Color1 == Color2. % check that both are the same.
 
 
 up(Node, Next):-
-  get_board(Board),
-  get_n(M),
-  nth0(Indx, Board, Node),
-  Node = [X1, Y1, Color1],
-  Indx >= M,
-  NextIndx is Indx - M,
-  nth0(NextIndx, Board, Next),
-  Next = [X2, Y2, Color2],
-  write("up"), nl,
-  write(Node), write(" "), write(Next), write(" "), nl,
-  Color1 == Color2.
-%   write(Next),nl.
+    get_board(Board), % get the board.
+    get_n(M), % get M (number of columns).
+    nth0(Indx, Board, Node), % get the index of the current node in the board.
+    Node = [_, _, Color1], % get the Color of the current node.
+    Indx >= M, % check that the current node's index is greater than or equal the number of columns.
+    NextIndx is Indx - M, % calculate index of the up node which will be current index - number of columns.
+    nth0(NextIndx, Board, Next), % get the index of the next node.
+    Next = [_, _, Color2], % get the Color of the nextNode.
+    Color1 == Color2. % check that both are the same.
 
 
 down(Node, Next):-
-  get_board(Board),
-  get_n(N),
-  get_m(M),
-  nth0(Indx, Board, Node),
-  Node = [X1, Y1, Color1],
-  CheckIndx is (N * M) - M,
-  Indx =< CheckIndx,
-  NextIndx is Indx + M,
-  nth0(NextIndx, Board, Next),
-  Next = [X2, Y2, Color2],
-  write("down"), nl,
-  write(Node), write(" "), write(Next), write(" "), nl,
-  Color1 == Color2.
-%   write(Next),nl.
+    get_board(Board), % get the board.
+    get_n(N), % get N (number of raws).
+    get_m(M), % get M (number of columns).
+    nth0(Indx, Board, Node), % get the index of the current node in the board.
+    Node = [_, _, Color1], % get the Color of the current node.
+    CheckIndx is (N * M) - M, % calculate (number of raws - number of columns) - numer of columns.
+    Indx =< CheckIndx, % check that current indx is less than or equal the CheckIndx (not in the last raw).
+    NextIndx is Indx + M, % calculate index of the down node which will be current indx + number of columns.
+    nth0(NextIndx, Board, Next), % get the index of the next node.
+    Next = [_, _, Color2], % get the Color of the nextNode.
+    Color1 == Color2. % check that both are the same.
 
 
 % predicate which will start the search.
 start(Board, N, M):-
-  set_n(N),
-  set_m(M),
-  set_board(Board),
-  End is N*M,
-  loop(Board, 1, End, 1).
+  set_n(N), % set the value of N.
+  set_m(M),  % set the value of M.
+  set_board(Board),  % set the value of Board.
+  End is N*M, % calculate the End of the loop.
+  loop(Board, 1, End).
 
 
-% Base case: When Start > End, we stop.
-loop(Board, End, End, _Step) :- !.
+% Base case: When Start == End -> we stop.
+loop(_, End, End) :- !.
 
 
-% Recursive case: Print Start, then increment Start and continue.
-loop([CurrentNode|Rest], Start, End, Step) :-
-     write("done"), nl,
-    Start < End,
-    search([CurrentNode], [], []),
-    Next is Start + Step,
-    loop(Rest, Next, End, Step).
-
+% Recursive loop
+loop([CurrentNode|Rest], Start, End) :-
+    Start =< End, % check that Start less than or equal end.
+    search([CurrentNode], [], []), % send to the search predicate the open list with the current node and empty closed list and empty cycle list.
+    Next is Start + 1, % increase the Next by 1.
+    loop(Rest, Next, End). % recursive call to the loop with the Rest of the Board. 
 
 
 
-% Node = [X, Y, Color]
-% Open and Closed lists = [[X, Y, Color], [X, Y, Color], ....]
-% board = [[X, Y, Color], [X, Y, Color], [X, Y, Color], [X, Y, Color], [X, Y, Color], [X, Y, Color], ....]
-% goal -> when the open list is empty.
-
-% board = [[0, 0, yellow], [0, 1, yellow], [0, 2, yellow], [0, 3, red], [1, 0, blue], [1, 1, yellow], [1, 2, blue], [1, 3, yellow], [2, 0, blue], [2, 1, blue], [2, 2, blue], [2, 3, yellow], [3, 0, blue], [3, 1, blue], [3, 2, blue], [3, 3, yellow]]
+% TEST CASES
+% start([[0, 0, blue], [0, 1, blue], [0, 2, blue], [0, 3, blue], [1, 0, blue], [1, 1, blue], [1, 2, yellow], [1, 3, yellow], [2, 0, red], [2, 1, red], [2, 2, yellow], [2, 3, yellow], [3, 0,red], [3, 1, red], [3, 2, yellow], [3, 3, yellow]], 4, 4).
 % start([[0, 0, yellow], [0, 1, yellow], [0, 2, yellow], [0, 3, red], [1, 0, blue], [1, 1, yellow], [1, 2, blue], [1, 3, yellow], [2, 0, blue], [2, 1, blue], [2, 2, blue], [2, 3, yellow], [3, 0, blue], [3, 1, blue], [3, 2, blue], [3, 3, yellow]], 4, 4).
-
-% start([[0, 0, yellow], [0, 1, yellow], [0, 2, yellow], [1, 0, blue], [1, 1, blue], [1, 2, red], [2, 0, blue], [2, 1, blue], [2, 2, red]], 3, 3).
+% start([[0, 0, yellow], [0, 1, yellow], [0, 2, yellow], [0, 3, red], [1, 0, blue], [1, 1, yellow], [1, 2, blue], [1, 3, yellow], [2, 0, blue], [2, 1, red], [2, 2, blue], [2, 3, yellow], [3, 0, blue], [3, 1, red], [3, 2, blue], [3, 3, yellow]], 4, 4).
+% start([[0, 0, blue], [0, 1, blue], [0, 2, blue], [0, 3, blue], [1, 0, blue], [1, 1, blue], [1, 2, blue], [1, 3, blue], [2, 0, blue], [2, 1, blue], [2, 2, blue], [2, 3, blue], [3, 0,red], [3, 1, red], [3, 2, red], [3, 3, red]], 4, 4).
+% start([[0, 0, blue], [0, 1, blue], [0, 2, yellow], [1, 0, blue], [1, 1, blue], [1, 2, yellow], [2, 0, yellow], [2, 1, yellow], [2, 2, yellow]], 3, 3).
+% start([[0, 0, blue], [0, 1, blue], [0, 2, yellow], [1, 0, blue], [1, 1, yellow], [1, 2, yellow], [2, 0, yellow], [2, 1, yellow], [2, 2, yellow]], 3, 3).
+% start([[0, 0, blue], [0, 1, blue], [0, 2, yellow], [1, 0, blue], [1, 1, red], [1, 2, yellow], [2, 0, yellow], [2, 1, yellow], [2, 2, yellow]], 3, 3).
